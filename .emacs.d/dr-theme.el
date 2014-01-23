c34f47b900bb579711ebe63d40069473de7eb266
Dale Roberts <dale.o.roberts@gmail.com>
2014-01-23 19:01:41 +1100

    more evil emacs customisations

---
+++ .emacs.d/dr-theme.el
@@ -0,0 +1,181 @@
+;;; dr-theme.el --- Custom theme for faces
+
+;;; Code:
+
+(deftheme dr
+  "Face colors similar to markers on a whiteboard.")
+
+(defvar font-lock-exception-face (make-face 'font-lock-exception-face))
+
+(setf python-font-lock-keywords
+  ;; Keywords
+  `(,(rx symbol-start
+         (or
+          "and" "del" "from" "not" "while" "as" "elif" "global" "or" "with"
+          "assert" "else" "if" "pass" "yield" "break" "except" "import" "class"
+          "in" "raise" "continue" "finally" "is" "return" "def" "for" "lambda"
+          "try"
+          ;; Python 2:
+          "print" "exec"
+          ;; Python 3:
+          ;; False, None, and True are listed as keywords on the Python 3
+          ;; documentation, but since they also qualify as constants they are
+          ;; fontified like that in order to keep font-lock consistent between
+          ;; Python versions.
+          "nonlocal")
+         symbol-end)
+    ;; functions
+    (,(rx symbol-start "def" (1+ space) (group (1+ (or word ?_))))
+     (1 font-lock-function-name-face))
+    ;; classes
+    (,(rx symbol-start "class" (1+ space) (group (1+ (or word ?_))))
+     (1 font-lock-type-face))
+    ;; Constants
+    (,(rx symbol-start
+          (or "Ellipsis" "False" "None" "NotImplemented" "True" "__debug__" "self")
+          symbol-end) . font-lock-constant-face)
+    ;; Decorators.
+    (,(rx line-start (* (any " \t")) (group "@" (1+ (or word ?_))
+                                            (0+ "." (1+ (or word ?_)))))
+     (1 font-lock-type-face))
+    ;; Builtin Exceptions
+    (,(rx symbol-start
+          (or
+           "ArithmeticError" "AssertionError" "AttributeError" "BaseException"
+           "DeprecationWarning" "EOFError" "EnvironmentError" "Exception"
+           "FloatingPointError" "FutureWarning" "GeneratorExit" "IOError"
+           "ImportError" "ImportWarning" "IndexError" "KeyError"
+           "KeyboardInterrupt" "LookupError" "MemoryError" "NameError"
+           "NotImplementedError" "OSError" "OverflowError"
+           "PendingDeprecationWarning" "ReferenceError" "RuntimeError"
+           "RuntimeWarning" "StopIteration" "SyntaxError" "SyntaxWarning"
+           "SystemError" "SystemExit" "TypeError" "UnboundLocalError"
+           "UnicodeDecodeError" "UnicodeEncodeError" "UnicodeError"
+           "UnicodeTranslateError" "UnicodeWarning" "UserWarning" "VMSError"
+           "ValueError" "Warning" "WindowsError" "ZeroDivisionError"
+           ;; Python 2:
+           "StandardError"
+           ;; Python 3:
+           "BufferError" "BytesWarning" "IndentationError" "ResourceWarning"
+           "TabError")
+          symbol-end) . font-lock-exception-face)
+    ;; Builtins
+    (,(rx symbol-start
+          (or
+           "abs" "all" "any" "bin" "bool" "callable" "chr" "classmethod"
+           "compile" "complex" "delattr" "dict" "dir" "divmod" "enumerate"
+           "eval" "filter" "float" "format" "frozenset" "getattr" "globals"
+           "hasattr" "hash" "help" "hex" "id" "input" "int" "isinstance"
+           "issubclass" "iter" "len" "list" "locals" "map" "max" "memoryview"
+           "min" "next" "object" "oct" "open" "ord" "pow" "print" "property"
+           "range" "repr" "reversed" "round" "set" "setattr" "slice" "sorted"
+           "staticmethod" "str" "sum" "super" "tuple" "type" "vars" "zip"
+           "__import__"
+           ;; Python 2:
+           "basestring" "cmp" "execfile" "file" "long" "raw_input" "reduce"
+           "reload" "unichr" "unicode" "xrange" "apply" "buffer" "coerce"
+           "intern"
+           ;; Python 3:
+           "ascii" "bytearray" "bytes" "exec"
+           ;; Extra:
+           "__all__" "__doc__" "__name__" "__package__")
+          symbol-end) . font-lock-builtin-face)
+    ;; assignments
+    ;; support for a = b = c = 5
+    (,(lambda (limit)
+        (let ((re (python-rx (group (+ (any word ?. ?_)))
+                             (? ?\[ (+ (not (any  ?\]))) ?\]) (* space)
+                             assignment-operator)))
+          (when (re-search-forward re limit t)
+            (while (and (python-syntax-context 'paren)
+                        (re-search-forward re limit t)))
+            (if (and (not (python-syntax-context 'paren))
+                     (not (equal (char-after (point-marker)) ?=)))
+                t
+              (set-match-data nil)))))
+     (1 font-lock-variable-name-face nil nil))
+    ;; support for a, b, c = (1, 2, 3)
+    (,(lambda (limit)
+        (let ((re (python-rx (group (+ (any word ?. ?_))) (* space)
+                             (* ?, (* space) (+ (any word ?. ?_)) (* space))
+                             ?, (* space) (+ (any word ?. ?_)) (* space)
+                             assignment-operator)))
+          (when (and (re-search-forward re limit t)
+                     (goto-char (nth 3 (match-data))))
+            (while (and (python-syntax-context 'paren)
+                        (re-search-forward re limit t))
+              (goto-char (nth 3 (match-data))))
+            (if (not (python-syntax-context 'paren))
+                t
+              (set-match-data nil)))))
+     (1 font-lock-variable-name-face nil nil))))
+
+(let ((class '((class color) (min-colors 89))))
+  (custom-theme-set-faces
+   'dr
+   `(Info-title-1-face ((,class (:family "helv" :weight bold :height 1.728))))
+   `(Info-title-2-face ((,class (:family "helv" :weight bold :height 1.44))))
+   `(Info-title-3-face ((,class (:family "helv" :weight bold :height 1.2))))
+   `(Info-title-4-face ((,class (:family "helv" :weight bold))))
+   `(compilation-column-number ((,class (:foreground "DarkGreen"))))
+   `(compilation-error ((,class (:foreground "Red1"))))
+   `(compilation-info ((,class (:weight normal :foreground "DeepSkyBlue4"))))
+   `(compilation-line-number ((,class (:foreground "DarkGreen"))))
+   `(cursor ((,class (:background "#000000"))))
+   `(default ((,class (:background "white" :foreground "black"))))
+   `(dired-marked ((,class (:background "dodgerblue3" :foreground "white"))))
+   `(flycheck-fringe-info ((,class (:background nil :foreground "#dd1144"))))
+   `(flycheck-fringe-warning ((,class (:background nil :foreground "#dd1144"))))
+   `(flycheck-fringe-error ((,class (:background nil :foreground "#dd1144"))))
+   `(flycheck-info ((,class (:background nil))))
+   `(flycheck-warning ((,class (:background nil))))
+   `(flycheck-error ((,class (:background nil))))
+   `(font-lock-exception-face ((,class (:foreground "#990000" :weight bold))))
+   `(font-lock-builtin-face ((,class (:foreground "#0086b3"))))
+   `(font-lock-comment-delimiter-face ((,class (:foreground "gray50"))))
+   `(font-lock-comment-face ((,class (:foreground "#999988"))))
+   `(font-lock-constant-face ((,class (:foreground "#999999"))))
+   `(font-lock-doc-face ((,class (:foreground "peru"))))
+   `(font-lock-doc-string-face ((,class (:foreground "peru"))))
+   `(font-lock-function-name-face ((,class (:foreground "#990000" :weight bold))))
+   `(font-lock-keyword-face ((,class (:foreground "#333333" :weight bold))))
+   `(font-lock-preprocessor-face ((,class (:foreground "#000000"))))
+   `(font-lock-reference-face ((,class (:foreground "#000000"))))
+   `(font-lock-regexp-grouping-backslash ((,class (:weight bold))))
+   `(font-lock-regexp-grouping-construct ((,class (:weight bold))))
+   `(font-lock-string-face ((,class (:foreground "#DD1144"))))
+   `(font-lock-type-face ((,class (:foreground "#445588" :weight bold))))
+   `(font-lock-variable-name-face ((,class (:foreground "#000000"))))
+   `(font-lock-warning-face ((,class (:foreground "red"))))
+   `(fringe ((,class (:background "#ffffff"))))
+   `(highlight ((,class (:background "gainsboro"))))
+   `(ido-first-match ((,class (:weight normal :foreground "DarkOrange3"))))
+   `(ido-only-match ((,class (:foreground "SeaGreen4"))))
+   `(ido-subdir ((,class (:foreground nil :inherit font-lock-keyword-face))))
+   `(info-header-node ((,class (:foreground "DeepSkyBlue1"))))
+   `(info-header-xref ((,class (:foreground "SeaGreen2"))))
+   `(info-menu-header ((,class (:family "helv" :weight bold))))
+   `(info-node ((,class (:foreground "DeepSkyBlue1"))))
+   `(info-xref ((,class (:foreground "SeaGreen4"))))
+   `(isearch ((,class (:background "gray"))))
+   `(isearch-lazy-highlight-face ((,class (:background "#cccccc"))))
+   `(lazy-highlight ((,class (:background "#eeeeee"))))
+   `(match ((,class (:background "LightPink1"))))
+   `(minibuffer-prompt ((,class (:foreground "black"))))
+   `(mode-line ((,class (:background "gray75" :foreground "black" ))))
+   `(mode-line-buffer-id ((,class (:weight bold :background nil :foreground "black"))))
+   `(mode-line-inactive ((,class (:background "gray75" :foreground "black" ))))
+   `(outline-1 ((,class (:foreground "Blue3"))))
+   `(outline-2 ((,class (:foreground "DodgerBlue"))))
+   `(outline-3 ((,class (:foreground "SteelBlue"))))
+   `(outline-4 ((,class (:foreground "RoyalBlue"))))
+   `(outline-5 ((,class (:foreground "DeepSkyBlue"))))
+   `(primary-selection ((,class (:background "blue3"))))
+   `(region ((,class (:background "gray"))))
+   `(show-paren-match-face ((,class (:background "dodgerblue1" :foreground "white"))))
+   `(show-paren-mismatch-face ((,class (:background "red1" :foreground "white"))))
+   `(warning ((,class (:foreground "Yellow4"))))))
+
+(provide-theme 'dr)
+
+;;; dr-theme.el ends here
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,22 +1,73 @@
-;; personal information
-
 (setf user-full-name "Dale Roberts")
 (setf user-mail-address "dale.o.roberts@gmail.com")
 
-(setf make-backup-files nil)
-(setq-default indent-tabs-mode nil)
-(setf require-final-newline t)
+(setq display-time-string-forms '((format-time-string "%H:%M" now)))
+(display-time-mode 1)
+
+(setenv "PATH" (concat "/usr/texbin:" (getenv "PATH")))
 
 ;; package archives
 
 (package-initialize)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+;; 80 column
+
+(require 'fill-column-indicator)
+(setq fci-rule-use-dashes nil)
+(setq fci-always-use-textual-rule nil)
+(setq fci-rule-width 1)
+(setq fci-rule-color "gray")
+
+;; Use the short version for yes/no
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; IBuffer
+
+(setq ibuffer-formats
+      '((mark modified read-only " "
+              (name 50 50 :left :elide) " "
+              filename-and-process)
+        (mark " " (name 16 -1) " " filename)))
+
+;; Undo tree
+
+(require 'undo-tree)
+(global-undo-tree-mode 1)
+
+;; python
+
+(defun my-prog-mode-hook
+  (setq fill-column 72)
+  (set (make-local-variable 'comment-auto-fill-only-comments) t)
+  (auto-fill-mode t))
+
+(add-hook 'python-mode-hook 'my-prog-mode-hook)
+(add-hook 'python-mode-hook 'jedi:setup)
+(add-hook 'python-mode-hook 'fci-mode)
+(setq jedi:complete-on-dot t)
+
+;; quit minibuffer
+
+(defun minibuffer-keyboard-quit ()
+  "Abort recursive edit. In Delete Selection mode, if the mark is active, just deactivate it;
+then it takes a second \\[keyboard-quit] to abort the minibuffer."
+  (interactive)
+  (if (and delete-selection-mode transient-mark-mode mark-active)
+      (setq deactivate-mark  t)
+    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+    (abort-recursive-edit)))
 
 ;; evil
 
-(add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
+
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
 
 (setq evil-esc-delay 0)
 (setq evil-default-cursor 'bar)
@@ -24,6 +75,44 @@
 (setq evil-visual-state-cursor 'hollow)
 (setq evil-replace-state-cursor 'box)
 
+(define-key evil-normal-state-map (kbd "<s-return>") 'toggle-frame-fullscreen)
+(define-key evil-normal-state-map (kbd "<SPC>") 'isearch-forward)
+(define-key evil-normal-state-map (kbd "Q") 'fill-paragraph)
+(define-key evil-normal-state-map (kbd ";") 'evil-ex)
+(define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+(define-key evil-normal-state-map ",b" 'ibuffer)
+(define-key evil-normal-state-map ",g" 'jedi:goto-definition)
+(define-key evil-normal-state-map (kbd "K") 'jedi:show-doc)
+(define-key evil-visual-state-map (kbd ";") 'evil-ex)
+(define-key evil-motion-state-map (kbd ";") 'evil-ex)
+
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+
+(setq key-chord-two-keys-delay 0.5)
+(key-chord-define evil-normal-state-map "ee" 'eval-buffer)
+(key-chord-mode 1)
+
+;; surround
+
+(require 'surround)
+(global-surround-mode 1)
+
+;; term
+
+(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+;; other
+
+(setf make-backup-files nil)
+(setq-default indent-tabs-mode nil)
+(setf require-final-newline t)
 (global-set-key (kbd "RET") 'newline-and-indent)
 
 ;; flycheck
@@ -32,9 +121,9 @@
 
 ;; interface
 
+(set-frame-font "Source Code Pro 13")
 (set-scroll-bar-mode nil)
 (tool-bar-mode 0)
-(menu-bar-mode 0)
 (setf inhibit-startup-screen t
       inhibit-startup-message t
       inhibit-startup-echo-area-message t)
@@ -47,3 +136,45 @@
 (add-hook 'latex-mode-hook 'flyspell-mode)
 (add-hook 'latex-mode-hook 'variable-pitch-mode)
 (add-hook 'latex-mode-hook 'auto-fill-mode)
+(fset 'font-latex-fontify-script nil)
+(fset 'tex-font-lock-subscript 'ignore)
+
+(setq font-latex-fontify-sectioning 'color)
+(setq font-latex-script-display (quote (nil)))
+(setq font-latex-deactivated-keyword-classes
+      '("italic-command" "bold-command" "italic-declaration" "bold-declaration"))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-info ((t nil)))
+ '(font-latex-subscript-face ((t nil)) t)
+ '(font-latex-superscript-face ((t nil)) t))
+
+(add-hook 'LaTeX-mode-hook (lambda ()
+  (push
+    '("makepdf" "makepdf2 %n %o %b" TeX-run-TeX nil t
+      :help "Run makepdf on file")
+    TeX-command-list)))
+
+(add-hook 'TeX-mode-hook '(lambda () (setq TeX-command-default "makepdf")))
+
+(defun TeX-texify ()
+  (interactive)
+  (save-buffer)
+  (TeX-command-menu "makepdf"))
+
+(evil-leader/set-key-for-mode 'latex-mode "r" 'TeX-texify)
+
+;;(load "server")
+;;(unless (server-running-p) (server-start))
+
+;; gitgutter
+
+(global-git-gutter-mode +1)
+
+;; color theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d")
+(load-theme 'dr t)
