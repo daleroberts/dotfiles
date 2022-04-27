b9c1b7a51635e12b9fac7d1efaae4a14bead2c17
Dale Roberts <dale.o.roberts@gmail.com>
2022-04-27 19:48:41 +1000

    early init

---
+++ .emacs.d/early-init.el
@@ -0,0 +1,8 @@
+(setq default-frame-alist
+       '((height . 56)
+         (width . 100)
+         (left . 100)
+         (top . 35)
+         (vertical-scroll-bars . nil)
+         (horizontal-scroll-bars . nil)
+         (tool-bar-lines . 0)))
